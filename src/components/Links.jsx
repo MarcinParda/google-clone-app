ddbe628d618752a0bd5bf06bdad8ff1708f02927
MarcinParda <marcin98parda@gmail.com>
2021-12-09 19:19:03 +0100

    add a key to links rendering

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -10,8 +10,8 @@ const links = [
 
 export const Links = () => (
   <div className="flex sm:justify-around justify-between items-center mt-4">
-    {links.map(({ url, text }) => (
-      <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
+    {links.map(({ url, text }, index) => (
+      <NavLink key={index} to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
     ))}
   </div>
 );
